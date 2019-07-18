3841b5aefff7ba814ba200feec140209720f4635
KyznecSPM <eme.di@ya.ru>
2019-07-18 23:17:08 +0300

    frame of the project

---
+++ src/services/bookstore-service.js
@@ -0,0 +1,21 @@
+export default class BookstoreService {
+  getBooks() {
+    return [
+      {
+        id: 1,
+        title: 'Production-Ready Microservices',
+        author: 'Susan J. Fowler',
+      },
+      {
+        id: 2,
+        title: 'Release It!',
+        author: 'Michael T. Nygard',
+      },
+      {
+        id: 3,
+        title: 'Spartak',
+        author: 'Raffaello Giovagnoli',
+      },
+    ];
+  }
+}
