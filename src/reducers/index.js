1b48a52aade5bda187bdef0797a2ab8731c83774
KyznecSPM <eme.di@ya.ru>
2019-08-19 23:35:50 +0300

    connect shopping-cart-table to redux-store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,24 +2,33 @@ const initialState = {
   books: [],
   loading: true,
   error: null,
+  cartItems: [
+    { id: 1, name: 'book1', count: 3, total: 150 },
+    { id: 2, name: 'book2', count: 2, total: 200 },
+    { id: 3, name: 'book1', count: 1, total: 50 },
+  ],
+  orderTotal: 220,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_BOOKS_REQUEST':
       return {
+        ...state,
         books: [],
         loading: true,
         error: null,
       };
     case 'FETCH_BOOKS_SUCCESS':
       return {
+        ...state,
         books: action.payload,
         loading: false,
         error: null,
       };
     case 'FETCH_BOOKS_FAILURE':
       return {
+        ...state,
         books: [],
         loading: false,
         error: action.payload,
